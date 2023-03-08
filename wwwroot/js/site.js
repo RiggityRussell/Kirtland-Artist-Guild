76cf027ffad81e920e7f411ddd6e1561e8c29a3b
Skitten168 <kittens@mail.nmc.edu>
2023-03-07 21:08:27 -0500

    Added Image slider at top of all pages (sample images as placeholders). Added social media links to footer. Centered navbar. Made 'Kirtland Artist Guild' text at top of page a link back to index.html and updated font. (left the other link as well, we can remove if we like this). can't remember if I did anything  else

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    var images = $('.slider-image');
+    var imageIndex = 0;
+
+    // Set the first image as active
+    images.first().addClass('active');
+
+    // Function to loop through images
+    function nextImage() {
+        images.eq(imageIndex).removeClass('active');
+        imageIndex = (imageIndex + 1) % images.length;
+        images.eq(imageIndex).addClass('active');
+    }
+
+    // Loop through images every 5 seconds
+    setInterval(nextImage, 5000);
+});
+
