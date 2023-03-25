1752f42489dfe11f9bd37e8e905dc28db3828ce8
freelancer09 <2009.mike@gmail.com>
2023-03-25 01:58:38 -0400

    Artist detail update

---
+++ Models/ArtistViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Kirtland_Artist_Guild.Models
+{
+    public class ArtistViewModel
+    {
+        public User User { get; set; }
+        public List<Art> Arts { get; set; }
+        public List<ArtImage> ArtImages { get; set; }
+    }
+}
